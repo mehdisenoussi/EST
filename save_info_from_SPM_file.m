2e13ff965d206766e4b3c9f25f6cc1ccda8453e6
Mehdi <mehdi@Mehdis-MacBook-Pro.local>
2019-06-01 12:41:07 +0200

    adding first scripts to get information from the SPM.mat file and to perform a first searchlight analysis in the native space

---
+++ save_info_from_SPM_file.m
@@ -0,0 +1,16 @@
+% change this path to the location where the preprocessed data are
+data_path = '/Volumes/MEHDIFAT/clayfmri/neuralData/';
+% get all the directories there, they represent each subject
+list = dir(data_path);
+list = list(4:end);
+
+% for each subject directory get the SPM.mat file which contains the variable Vbeta.
+% Vbeta holds the information on which beta (e.g. beta103.nii) "represents what"
+% e.g. beta103.nii is the action 3 (stirring) for a coffee making sequence with water first.
+% we the save the Vbeta structure to be able to load it with Python in the searchlight analysis script
+for li = list'
+    load([data_path, li.name, '/GLM/SPM.mat'])
+    Vbeta = SPM.Vbeta;
+    % make sure to change this path
+    save(sprintf('/Users/mehdi/work/ghent/danesh/code/Vbeta_%s.mat', li.name), 'Vbeta');
+end
